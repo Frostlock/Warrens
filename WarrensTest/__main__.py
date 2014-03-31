36664604154b92a9796045d12010a83b748d2187
Frostlock <pieter.boogaerts@gmail.com>
2014-03-31 21:49:19 +0200

    filtered out pyc files

---
+++ WarrensTest/__main__.py
@@ -0,0 +1,11 @@
+'''
+Created on Mar 22, 2014
+
+@author: pi
+'''
+
+if __name__ == '__main__':
+    #test the config file sanity
+    #TODO: this works in eclipse but fails in commandline...
+    import test_config
+    test_config.runTests()
