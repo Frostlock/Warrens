7b2d953f7610e5947835ca4c240edb4e41c51df7
Frostlock <pieter.boogaerts@gmail.com>
2014-04-13 11:49:37 +0200

    Some Git tests and adding skeleton test file for GUI.

---
+++ WarrensTest/Gui_test.py
@@ -0,0 +1,43 @@
+'''
+Created on Apr 13, 2014
+
+@author: pi
+
+This module runs tests on the GUI.
+TODO: Implement some GUI tests.
+'''
+import unittest
+
+class TestGui(unittest.TestCase):
+    
+    @classmethod
+    def setUpClass(self):
+        """
+        unittest framework will run this once before all the tests in this class.
+        """
+        pass
+    
+    @classmethod
+    def tearDownClass(self):
+        """
+        unittest framework will run this once after all the tests in this class have been run.
+        """
+        pass
+    
+    def setUp(self):
+        """
+        unittest framework will run this before every individual test.
+        """
+        pass
+        
+    def tearDown(self):
+        """
+        unittest framework will run this after every individual test.
+        """
+        pass
+    
+    def test_gui(self):
+        """
+        Test to implement.
+        """
+        pass
