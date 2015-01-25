21879a8bb0befba239a99fca87ec3bb9d0cd333d
Frostlock <pieter.boogaerts@gmail.com>
2015-01-25 09:32:04 +0100

    Launcher tweaks

--- Launcher.py
+++ Launcher.py
@@ -4,9 +4,10 @@ from WarrensGUI.WarrensOpenGL import GlApplication
 #This is where it all starts!
 if __name__ == '__main__':
     print 'Select option:'
-    print ' 0: 2D interface'
-    print ' 1: 3D interface'
+    print ' 0: 2D pygame interface'
+    print ' 1: 3D pygame opengl interface (needs pyopengl)'
     
+    #ans = 1
     ans = input()
     if ans == 0:
         _application = GuiApplication()
